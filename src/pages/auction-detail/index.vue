2ae63766817276dcf049d5230c491cb1a643cb86
JokerPoor <17369427086@163.com>
2024-11-30 01:51:10 +0800

    feat: 部分静态页

--- src/pages/auction-detail/index.vue
+++ src/pages/auction-detail/index.vue
@@ -1,120 +1,84 @@
+
 <template>
-  <div class="container">
-    <div class="header flex justify-between items-center">
-      <div class="icon-circle">
-        <van-icon color="black" name="arrow-left" />
-      </div>
-      <div class="icon-circle">
-        <van-icon color="black" name="star" />
-      </div>
+  <div
+    class="auction-detail px-4 pb-10 min-h-screen flex flex-col justify-between"
+    :style="{
+      'background-image':
+        'url(https://fastly.jsdelivr.net/npm/@vant/assets/cat.jpeg)',
+    }"
+  >
+    <div class="flex justify-between px-2 pt-8">
+      <van-button round type="primary">
+        <i class="iconfont icon-back" @click="$router.back()"></i>
+      </van-button>
+      <van-button round type="primary">
+        <i class="iconfont icon-collection"></i>
+      </van-button>
     </div>
     <div>
-      <van-image
-        src="https://fastly.jsdelivr.net/npm/@vant/assets/cat.jpeg"
-      ></van-image>
-    </div>
-    <div class="content">
-      <h2>Hachiman no Hogo</h2>
-      <p>Tosei-gusoku Samurai Armour</p>
-      <div class="badge">#0123</div>
-      <div class="stars">
-        <van-rate color="#000" :model-value="3"></van-rate>
+      <p class="text-black font-800 text-3xl">Hachiman no Hogo</p>
+      <p class="text-black text-xl">Tosei-gusoku Samurai Armour Cat Beautify</p>
+      <van-tag class="my-2" type="primary">#0123</van-tag>
+      <div class="my-4">
+        <van-rate
+          :model-value="2"
+          icon="like"
+          void-icon="like-o"
+          color="#121212"
+        />
       </div>
-      <div class="timer">01:36:18</div>
+      <div class="text-6xl font-800 text-black">01:36:18</div>
 
-      <div class="images flex justify-between">
-        <div class="image" v-for="i in 4" :key="i"></div>
+      <div class="overflow-x-auto w-full flex space-x-4">
+        <div class="flex-shrink-0" v-for="i in 10" :key="i">
+          <van-image
+            width="100"
+            height="100"
+            radius="20"
+            src="https://fastly.jsdelivr.net/npm/@vant/assets/cat.jpeg"
+          />
+        </div>
       </div>
 
-      <button class="bid-button">Place a Bid</button>
+      <van-button
+        type="primary"
+        block
+        round
+        size="small"
+        @click="placeABidHandle"
+        >Place A Bid</van-button
+      >
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import usePopup from "@/hooks/usePopup";
+import PlaceABidPopup from "./place-a-bid-popup/index.vue";
+
+function placeABidHandle() {
+  const popup = usePopup<{
+    isShouldClose: () => void;
+  }>({
+    props: {
+      isShouldClose: () => {
+        popup.close();
+      },
+    },
+    slots: {
+      default: () =>
+        h(PlaceABidPopup, {
+          close: () => popup.close(),
+        }),
+    },
+  });
+}
 </script>
 
-<style scoped>
-.container {
-  background-color: #f0f0f0;
-  width: 300px;
-  margin: 20px auto;
-  padding: 16px;
-  border-radius: 8px;
-  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-  text-align: center;
-}
-
-.header {
-  margin-bottom: 16px;
-}
-
-.icon-circle {
-  background-color: white;
-  border-radius: 50%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 32px;
-  height: 32px;
-}
-
-.content {
-  color: #333;
-}
-
-h2 {
-  font-size: 20px;
-  margin: 8px 0;
-  font-weight: bold;
-}
-
-p {
-  font-size: 14px;
-  margin-bottom: 8px;
-}
-
-.badge {
-  display: inline-block;
-  background-color: black;
-  color: white;
-  padding: 4px 8px;
-  border-radius: 4px;
-  font-size: 12px;
-  margin-bottom: 8px;
-}
-
-.stars {
-  margin: 8px 0;
-}
-
-.timer {
-  font-size: 24px;
-  margin: 8px 0;
-  font-weight: bold;
-}
-
-.images {
-  margin: 16px 0;
-}
-
-.image {
-  width: 50px;
-  height: 50px;
-  background-color: #ccc;
-  border-radius: 4px;
-  margin-right: 8px;
-}
-
-.bid-button {
-  background-color: black;
-  color: white;
-  padding: 12px;
-  width: 100%;
-  border: none;
-  border-radius: 4px;
-  font-size: 16px;
-  cursor: pointer;
-  margin-top: 16px;
+<style scoped lang="scss">
+.auction-detail {
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
 }
 </style>
